f6f5fe1140d5f2bb305e8758e2dc4f91227416c1
AlexnovichAS <n215401@yandex.ru>
2023-03-05 17:06:19 +0300

    Allure configured

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -1,12 +1,12 @@
 package StepDefinition;
 
 import io.cucumber.java.ru.И;
+import io.qameta.allure.Attachment;
 
 public class Steps {
 
     private static double x;
     private static double y;
-
 
     @И("^Получить первое число : (.*)$")
     public static void получить_первое_число(Double numberX) {
@@ -18,21 +18,23 @@ public class Steps {
         y = numberY;
     }
 
+    @Attachment(value = "Ответ", type = "text/html")
     @И("^Получить (сумму|разность|остаток|произведение)$")
-    public static void получить(String name) {
+    public static String получить(String name) {
         switch (name) {
             case "сумму":
                 System.out.println(x + y);
-                break;
+                return String.valueOf(x + y);
             case "разность":
                 System.out.println(x - y);
-                break;
+                return String.valueOf(x - y);
             case "остаток":
                 System.out.println(x / y);
-                break;
+                return String.valueOf(x / y);
             case "произведение":
                 System.out.println(x * y);
-                break;
+                return String.valueOf(x * y);
         }
+        return null;
     }
 }
